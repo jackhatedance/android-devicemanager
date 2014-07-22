a03586ee4a475f38ccd7f34728db8d35230615fd
jianghaoding <jianghaoding@gmail.com>
2014-07-22 18:03:42 +0800

    second time

--- MyDevices/src/com/deviceyun/mydevices/MainActivity.java
+++ MyDevices/src/com/deviceyun/mydevices/MainActivity.java
@@ -7,10 +7,18 @@ import java.util.Map;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.deviceyun.devicemanager.R;
 
 public class MainActivity extends ActionBarActivity {
 	List<Map<String, String>> planetsList = new ArrayList<Map<String, String>>();
@@ -38,8 +46,53 @@ public class MainActivity extends ActionBarActivity {
 
 		lv.setAdapter(simpleAdpt);
 
+		// React to user clicks on item
+		lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			public void onItemClick(AdapterView<?> parentAdapter, View view,
+					int position, long id) {
+
+				// We know the View is a TextView so we can cast it
+				TextView clickedView = (TextView) view;
+
+				Toast.makeText(
+						MainActivity.this,
+						"Item with id [" + id + "] - Position [" + position
+								+ "] - Planet [" + clickedView.getText() + "]",
+						Toast.LENGTH_SHORT).show();
+
+			}
+		});
+
+		// we register for the contextmneu       
+		registerForContextMenu(lv);
 	}
 
+	// We want to create a context Menu when the user long click on an item
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+
+		super.onCreateContextMenu(menu, v, menuInfo);
+		AdapterContextMenuInfo aInfo = (AdapterContextMenuInfo) menuInfo;
+
+		// We know that each row in the adapter is a Map
+		HashMap map = (HashMap) simpleAdpt.getItem(aInfo.position);
+
+		menu.setHeaderTitle("Options for " + map.get("planet"));
+		menu.add(1, 1, 1, "Details");
+		menu.add(1, 2, 2, "Delete");
+
+	}
+	
+	// This method is called when user selects an Item in the Context menu
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+	    int itemId = item.getItemId();
+	    // Implements our logic
+	    Toast.makeText(this, "Item id ["+itemId+"]", Toast.LENGTH_SHORT).show();
+	    return true;
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -65,7 +118,6 @@ public class MainActivity extends ActionBarActivity {
 		planetsList.add(createPlanet("planet", "电灯"));
 		planetsList.add(createPlanet("planet", "电视机"));
 		planetsList.add(createPlanet("planet", "热水器"));
-		
 
 	}
 
